108b2d646a5d5cee15b6a5dee7c43e7b620c96d1
ruize <ruize.nie@marinethinking.com>
2021-04-07 10:42:52 -0300

    first commit

---
+++ build/ros2_env/CMakeFiles/Hydrodynamics.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Hydrodynamics.dir/plugins/Gazebo.cc.o"
+  "CMakeFiles/Hydrodynamics.dir/plugins/Geometry.cc.o"
+  "CMakeFiles/Hydrodynamics.dir/plugins/PhysicalConstants.cc.o"
+  "CMakeFiles/Hydrodynamics.dir/plugins/Physics.cc.o"
+  "CMakeFiles/Hydrodynamics.dir/plugins/Utilities.cc.o"
+  "CMakeFiles/Hydrodynamics.dir/plugins/Wavefield.cc.o"
+  "CMakeFiles/Hydrodynamics.dir/plugins/WavefieldEntity.cc.o"
+  "libHydrodynamics.pdb"
+  "libHydrodynamics.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Hydrodynamics.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/ros2_env/launch/ros2_env_launch.py
@@ -0,0 +1,71 @@
+#/*******************************************************************************
+# *
+# *   Copyright 2019, Manufactura de Ingenios Tecnológicos S.L. 
+# *   <http://www.mintforpeople.com>
+# *
+# *   Redistribution, modification and use of this software are permitted under
+# *   terms of the Apache 2.0 License.
+# *
+# *   This software is distributed in the hope that it will be useful,
+# *   but WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND; without even the implied
+# *   warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# *   Apache 2.0 License for more details.
+# *
+# *   You should have received a copy of the Apache 2.0 License along with    
+# *   this software. If not, see <http://www.apache.org/licenses/>.
+# *
+# ******************************************************************************/
+import sys
+import os
+
+from ament_index_python.packages import get_package_share_directory, get_package_prefix
+from launch import LaunchDescription
+from launch.actions.execute_process import ExecuteProcess
+from launch_ros.actions import Node
+
+def generate_launch_description():
+
+    args = sys.argv[1:]
+
+    # sdf_robobo = os.path.join(get_package_share_directory('robobo_gazebo'), 'models/robobo/model.sdf')
+    # assert os.path.exists(sdf_robobo)
+
+    install_dir = get_package_prefix('ros2_env')
+
+    model_dir = os.path.join(get_package_share_directory('ros2_env'), 'models')
+
+    world_path = os.path.join(get_package_share_directory('ros2_env'), 'worlds', 'my_world.world')
+
+    if 'GAZEBO_MODEL_PATH' in os.environ:
+        os.environ['GAZEBO_MODEL_PATH'] =  os.environ['GAZEBO_MODEL_PATH'] + ':' + model_dir
+    else:
+        os.environ['GAZEBO_MODEL_PATH'] = model_dir
+
+    lib_dir = os.path.join(install_dir, 'lib')
+
+    if 'GAZEBO_PLUGIN_PATH' in os.environ:
+        os.environ['GAZEBO_PLUGIN_PATH'] = os.environ['GAZEBO_PLUGIN_PATH'] + ':' + lib_dir
+    else:
+        os.environ['GAZEBO_PLUGIN_PATH'] = os.path.join(install_dir, '/lib')
+
+    # try:
+    #     envs = {}
+    #     for key in os.environ.__dict__["_data"]:
+    #         key = key.decode("utf-8")
+    #         if (key.isupper()):
+    #             envs[key] = os.environ[key]
+    # except Exception as e:
+    #     print("Error with Envs: " + str(e))
+    #     return None
+
+    # robot_name = 'robobo'
+    # robot_namespace = 'robobo'
+    ld = LaunchDescription([
+        ExecuteProcess(
+            cmd=['gazebo', '--verbose', world_path], output='screen',
+            # env=envs
+        ),
+        # Node(package='robobo_gazebo', node_executable='spawn_robobo.py', 
+        #     arguments=[robot_name, robot_namespace, '0.0', '0.0', '2.0'], output='screen')
+    ])
+    return ld
